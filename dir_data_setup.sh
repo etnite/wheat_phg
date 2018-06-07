8db03ba7f1d7f1d19412cd1478644983c758b0ff
bward8 <brian.phillip.ward@gmail.com>
2018-06-07 18:59:11 -0400

    Slight change to dir_data_setup.sh

--- dir_data_setup.sh
+++ dir_data_setup.sh
@@ -8,21 +8,21 @@
 ## It takes two positional input parameters - one is the intended path of the
 ## project directory (absolute), and the second is the relative path of the directory
 ## within the git repository holding sample configuration files (this is likely
-## just "config")
+## just "config_files")
 
-proj_dir=$1
-config_dir=$2
+proj_dir="$1"
+config_dir="$2"
 
-config_dir=$(realpath ${config_dir})
+config_dir="$(realpath ${config_dir})"
 
 ## Make project directory, and subdirectories
-mkdir -p proj_dir
-cd $proj_dir
+mkdir -p "$proj_dir"
+cd "$proj_dir"
 mkdir ref config
 
 ## Copy the contents of the repository sample config files to the project
 ## config/ folder
-cp ${config_dir}/* config/
+cp "${config_dir}"/* config/
 
 ## Download v1 RefSeq and .gff3 annotation file
 wget -P ref/ http://people.beocat.ksu.edu/~kwjordan/161010_Chinese_Spring_v1.0_pseudomolecules_parts_renamed.fasta
