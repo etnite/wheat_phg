3162a287395b0d96cd0709f64300354d376fdffd
etnite <15957306+etnite@users.noreply.github.com>
2020-02-26 10:14:18 -0500

    Update gff3_to_flat_bed.sh

--- code/gff3_to_flat_bed.sh
+++ code/gff3_to_flat_bed.sh
@@ -19,6 +19,25 @@ set -e
 ## gff3 files even if they don't have the .gff3 extension
 ################################################################################
 
+
+#### SLURM job control #### 
+
+#SBATCH --job-name="gff2bed" #name of the job submitted
+#SBATCH --partition=short #name of the queue you are submitting job to
+  ##SBATCH --nodes=1 #Number of nodes
+#SBATCH --ntasks=1  #Number of overall tasks - overrides tasks per node
+  ##SBATCH --ntasks-per-node=22 #number of cores/tasks
+#SBATCH --time=1:00:00 #time allocated for this job hours:mins:seconds
+#SBATCH --mail-user=jane.doe@isp.com #enter your email address to receive emails
+#SBATCH --mail-type=BEGIN,END,FAIL #will receive an email when job starts, ends or fails
+#SBATCH --output="stdout.%j.%N" # standard out %j adds job number to outputfile name and %N adds the node name
+#SBATCH --error="stderr.%j.%N" #optional but it prints our standard error
+
+
+#### Executable ####
+
+module load bedtools
+
 gff_file=$1
 
 ## Remove the ".gff3.gz" for the output name
